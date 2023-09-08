1dfbaa40fa66a743471f5e6dbdf8b08a11bde8f1
Guilherme Andre <gui1andre89@gmail.com>
2023-09-08 14:48:06 -0300

    first commit

--- IQuiz/ViewController.swift
+++ IQuiz/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func clickButton(_ sender: Any) {
+        print("Botão Pressionado!")
+    }
+    
+    @IBOutlet weak var IniciadoButton: UIButton!
 }
 
